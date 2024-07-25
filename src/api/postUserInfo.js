5066080c7c8259492ef194dcf93ccce0801c49b2
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2024-07-25 11:05:49 +0400

    fix(postUserInfo): remove unneccessary symbol

--- src/api/postUserInfo.js
+++ src/api/postUserInfo.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const postUserInfo = async (url, params) => {
     try {
-        return await axios.post(url, params);;
+        return await axios.post(url, params);
     } catch (err) {
         return err.response.status;
     }
