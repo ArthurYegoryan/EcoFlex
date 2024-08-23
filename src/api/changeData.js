dab5b71ccba54472b65abe06181421bcd1032e6e
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2024-08-23 19:08:38 +0400

    fix(changeData): remove logs

--- src/api/changeData.js
+++ src/api/changeData.js
@@ -2,8 +2,6 @@ import axios from "axios";
 import { Headers } from "../constants/configs/configs";
 
 export const changeData = async (url, newData) => {
-    console.log("Body: ", JSON.stringify(newData, null, 3));
-
     try {
         return await axios.put(url, newData, Headers);
     } catch (err) {
